ad0f535697e11309b22d04f1908185c2fc6cf283
WillerNote <willerhenrique100@gmail.com>
2025-01-04 21:06:55 -0300

    feat: Adicionar novos estudos

---
+++ .venv/Exemplo1/TipoVariavel.py
@@ -0,0 +1,8 @@
+codigo = 10
+salario = 1500.00
+nome = 'José'
+situacao = True
+
+tipo = type (salario)
+
+print("Código: " + str (codigo) + " Nome: ", nome, "O salário atual é de R$: " + str(salario))
